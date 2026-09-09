adc66cf8ad6d27bad21b4ab7d4766f96392a4954
umeshp99-soft <umesh.thorat@p99soft.com>
2026-09-09 12:39:12 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz9_JjgFaG3ezCIA95Hr1b7QSFSWlBvq6z58UVNw3fHux2OrKkEers-QAKL5068JsQ/exec",
-
-  QUESTIONS_PER_ROUND: 5,
-
-  SECONDS_PER_QUESTION: 20
-};
