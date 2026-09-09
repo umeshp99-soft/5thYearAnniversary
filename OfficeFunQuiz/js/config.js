48a1cdd9a48977eccba0c7d388df7a559c7324f9
umeshp99-soft <umesh.thorat@p99soft.com>
2026-09-09 12:16:30 +0530

    Delete OfficeFunQuiz directory

--- OfficeFunQuiz/js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  // Deploy backend/Code.gs as a Web App and paste its /exec URL here.
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  QUESTIONS_PER_ROUND: 5,
-  SECONDS_PER_QUESTION: 20
-};
