9a45faff107c90f8ce13a360c7b5ab11257eba6c
umeshp99-soft <umesh.thorat@p99soft.com>
2026-09-10 14:52:25 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbwcQkE7R1wbsf72iCc-ChBrX_RPPWhssIMjWTfDt-U_7x6qKKFps--sD_9YBq14O_EN/exec",
+  SECONDS_PER_QUESTION: 20,
+  ROUNDS: [
+    { name: "Round 1", qualifiers: 20 },
+    { name: "Round 2", qualifiers: 9 },
+    { name: "Round 3", qualifiers: 3 },
+    { name: "Final", qualifiers: 1 }
+  ]
+};
